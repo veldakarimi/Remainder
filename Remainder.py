fcd495bb3fe055ecec3e69bd14ccd6889c3c73f1
Velda Kiara <veldakarimi@gmail.com>
2020-04-28 01:26:20 +0300

    changes to return value

--- Remainder.py
+++ Remainder.py
@@ -1,7 +1,7 @@
 # Write your remainder function here:
 def remainder(num1, num2):
-  num = (num1*num1) % (num2*(1/2))
-  return num
+  return (2*num1)%(num2/2)
+  
 # Uncomment these function calls to test your remainder function:
 print(remainder(15, 14))
 # should print 2
